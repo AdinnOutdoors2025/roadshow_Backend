81bec1bda927f31a4ad84865e45c9219d6107ea2
SathishKumar Dhanasekaran <srbedev@adinn.co.in>
2026-05-28 16:15:02 +0530

    full completed

--- Routes/roadshowQuotation/roadshowQuotationRoute.js
+++ Routes/roadshowQuotation/roadshowQuotationRoute.js
@@ -15,7 +15,7 @@ const router = express.Router();
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
-    fileSize: 25 * 1024 * 1024,
+    fileSize: 100 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     if (file.mimetype !== "application/pdf") {
--- VehicleMain.js
+++ VehicleMain.js
@@ -147,8 +147,8 @@ app.use(
 );
 
 app.use(bodyParser.json());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "public")));
