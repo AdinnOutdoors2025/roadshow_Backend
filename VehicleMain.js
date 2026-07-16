3ad8a8b7dc93166e5a54fdfbab08a806bb360ce4
karthika <reactdeveloper@adinn.co.in>
2026-07-16 18:51:48 +0530

    cors added

--- VehicleMain.js
+++ VehicleMain.js
@@ -121,6 +121,7 @@ if (process.env.STORAGE_TYPE !== "space") {
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:3002",
   "http://localhost:5173",
   "https://roadshowratecard.netlify.app",
   "http://192.168.2.159:3000",
